419363be65a628bf5a0c2d9925ad217958bace35
Ricardo Alarcon <geocodinglife@gmail.com>
2020-09-13 17:08:31 -0500

    it works

--- song.rb
+++ song.rb
@@ -1,40 +1,26 @@
 require 'ruby_linear_regression'
 require 'csv'
-require 'pp'
-require 'pry'
-
-class  TruthRevealing
-  attr_reader :songs
-  def initialize(data)
-    @songs = musification(data)
-  end
-
-  def revealing
-    regression = RubyLinearRegression.new
-    songs.each do |song|
-
-      regression.load_training_data(song.beats, song.popularity)
-
-      binding.pry
-      regression.train_normal_equation
-      puts "Trained model with the following cost fit #{regression.compute_cost}"
-      prediction_data = [50]
-      predicted_popularity = regression.predict(prediction_data)
-      puts "Predicted populatity: #{predicted_popularity.round}"
-    end
-  end
-
-  Song = Struct.new(:beats, :popularity)
-
-  def musification(data)
-    data.collect { |cell| Song.new([cell[1].to_i], cell[2].to_i) }
-  end
-end
-
 
 data = CSV.read('db/data.csv', encoding: 'iso-8859-1:utf-8')
 data.shift
 
-truth_revealing = TruthRevealing.new(data)
-pp truth_revealing.revealing
+width = []
+height = []
 
+data.each do |row|
+  width.push([row[1].to_i])
+  height.push(row[2].to_i)
+end
+
+p width
+p height
+
+linear_regression = RubyLinearRegression.new
+linear_regression.load_training_data(width, height)
+linear_regression.train_normal_equation
+
+puts "Trained model with the following cost fit #{linear_regression.compute_cost}"
+
+prediction_data = [50]
+predicted_popularity = linear_regression.predict(prediction_data)
+puts "Predicted populatity: #{predicted_popularity.round}"
